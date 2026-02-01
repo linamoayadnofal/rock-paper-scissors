3d87ff3a7d952edc788bd8c174370cc5b7797650
linamoayadnofal <leena2006_nofal@outlook.com>
2026-02-01 21:00:47 +0300

    Add files via upload

---
+++ RockPaperScissor.py
@@ -0,0 +1,51 @@
+#rock,paper,scissors game
+import random
+
+while True:
+    choices= ["rock","paper","scissors"]
+    computer= random.choice(choices)
+
+    player=None
+    while player not in choices:
+    
+     player=input("Enter your choice ").lower()
+     
+    if player==computer:
+            print("computer:",computer)
+            print("player:",player)
+            print("Tie!")
+    
+    elif player=="rock":
+        if computer=="scissors":
+             print("computer:",computer)
+             print("player:",player)
+             print("you win!")
+        if computer=="paper":
+             print("computer:",computer)
+             print("player:",player)
+             print("you lose!")  
+    
+    elif player=="scissors" :
+        if computer=="rock":
+            print("computer:",computer)
+            print("player:",player)
+            print("you lose!")  
+        if computer=="paper":
+            print("computer:",computer)
+            print("player:",player)
+            print("you win!")  
+            
+    elif player=="paper" :
+        if computer=="rock":
+            print("computer:",computer)
+            print("player:",player)
+            print("you win!")  
+        if computer=="scissors":
+            print("computer:",computer)
+            print("player:",player)
+            print("you lose!")  
+    
+    playAgain=input("play again? (yes/no): ").lower()
+    if playAgain != "yes":
+         break
+print("Bye!")
